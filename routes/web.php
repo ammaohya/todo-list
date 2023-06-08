54238ba9023a53ddbb51ce36e008767c3af38972
Amma Hou <lily29741@gmail.com>
2023-06-08 21:38:02 +0800

    modify page router and readme

--- routes/web.php
+++ routes/web.php
@@ -11,15 +11,7 @@
 |
 */
 
+
 Route::get('/', function () {
-    return view('welcome');
-});
-
-
-Route::get('/owo', function () {
     return view('index');
 });
-
-Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
